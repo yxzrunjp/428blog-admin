ff2983898cfc958d27a3fc00d4ee3a6af10212a5
yxzrunjp <yxz894894@gmail.com>
2023-02-19 23:30:11 +0800

    设置浏览器标题

--- src/router/index.js
+++ src/router/index.js
@@ -8,34 +8,59 @@ const routes = [
         redirect: '/blog/list',
         children: [
             {
+                meta: {
+                    name: "博客管理"
+                },
                 path: '/blog/list',
                 component: () => import('../views/Blog/BlogList.vue')
+
             },
             {
+                meta: {
+                    name: "分类管理"
+                },
                 path: '/blog/category',
                 component: () => import('../views/Blog/BlogCategory.vue')
             },
+            // {
+            //     meta:{
+            //         name:""
+            //     },
+            //     path: '/blog/test',
+            //     component: () => import('../views/Blog/Test.vue')
+            // },
             {
-                path: '/blog/test',
-                component: () => import('../views/Blog/Test.vue')
-            },
-            {
+                meta: {
+                    name: "专题管理"
+                },
                 path: '/special/list',
                 component: () => import('../views/Special/SpecialList.vue')
             },
             {
+                meta: {
+                    name: "个人信息"
+                },
                 path: '/settings/myinfo',
                 component: () => import('../views/Settings/MyInfo.vue')
             },
             {
+                meta: {
+                    name: "博客成员"
+                },
                 path: '/settings/user',
                 component: () => import('../views/Settings/User.vue')
             },
             {
+                meta: {
+                    name: "系统设置"
+                },
                 path: '/settings/sysinfo',
                 component: () => import('../views/Settings/SysInfo.vue')
             },
             {
+                meta: {
+                    name: "回收站"
+                },
                 path: '/recover/list',
                 component: () => import('../views/Recover/RecoveryList.vue')
             },
@@ -54,6 +79,7 @@ const router = createRouter({
 })
 
 router.beforeEach((to, from, next) => {
+    document.title = to.meta.name || 'blog'
     if (to.path !== '/login') {
         const cookiesUserInfo = cookies.get('userInfo')
         if (cookiesUserInfo) {
