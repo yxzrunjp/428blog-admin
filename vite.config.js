466189eeac7003e119d5da52eeb99e60ac1f5c2a
yxzrunjp <yxz894894@gmail.com>
2023-03-10 00:02:54 +0800

    更改配置

--- vite.config.js
+++ vite.config.js
@@ -1,30 +1,40 @@
 import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
-// console.log(path.resolve(__dirname,'../src'))
-// console.log(path.join(__dirname,'../src'))
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  server:{
-    hmr:true,//热更新
-    port:3001,
-    proxy:{
-      '/api':{
+  server: {
+    hmr: true,//热更新
+    port: 3001,
+    proxy: {
+      '/api': {
         // target:loadEnv(mode,process.cwd()).VITE_APP_PROXY_TARGET,//目标代理接口地址
-        target:"http://localhost:8081/",
-        secure:false,
-        changeOrigin:true,//开启代理，在本地创建一个虚拟服务端
-        pathRewrite:{
-          '^/api':'/api',
+        target: "http://localhost:8081/",
+        secure: false,
+        changeOrigin: true,//开启代理，在本地创建一个虚拟服务端
+        pathRewrite: {
+          '^/api': '/api',
         }
       }
     }
   },
-  resolve:{
+  resolve: {
     // 配置路径别名
-    alias:{
-      '@':path.resolve(__dirname,'./src')
+    alias: {
+      '@': path.resolve(__dirname, './src')
     }
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id.toString().split('node_modules/')[1].split('/')[0].toString();
+          }
+        }
+      }
+    }
+
   }
 })
